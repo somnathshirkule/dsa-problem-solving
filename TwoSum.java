937e7538a90ce90301165d3ab5a65d1cd88b31e7
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-12-08 20:13:35 +0530

    added solution1 with O(n) time complexity
    added solution1 with O(n) time complexity

--- TwoSum.java
+++ TwoSum.java
@@ -2,6 +2,8 @@ public class TwoSum{
     public static void main(String[] args){
         System.out.println(new TwoSum().solution(new int[]{2,7,11,15}, 9));
     }
+
+    //time complexity O(n^2)
     private int[] solution(int[] arr, int target){
         int[] tempArr = new int[2];
         for(int i = 0; i < arr.length; i++){
@@ -13,5 +15,24 @@ public class TwoSum{
             }
         }
         return tempArr;
+    }
+
+    //time complexity O(n)
+    private int[] solution1(int[] arr, int target){
+        int[] temp = new int[2];
+        Map<Integer, Integer> tempMap = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            tempMap.put(arr[i], i);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            int tempRem = target - arr[i];
+            if(tempMap.containsKey(tempRem) && tempMap.get(tempRem) != i){
+                temp[1] = i;
+                temp[0] = tempMap.get(tempRem);
+            }
+        }
+
+        return temp;
     }
 }
