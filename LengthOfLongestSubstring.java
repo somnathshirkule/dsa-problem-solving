d4d0ee9cce9856c74c31c7572bb5944010ab5d36
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-12-09 22:25:41 +0530

    Create LengthOfLongestSubstring.java

---
+++ LengthOfLongestSubstring.java
@@ -0,0 +1,25 @@
+public class LengthOfLongestSubstring{
+    //Time complexity => O(n)
+    //Space complexity => O(1)
+    public int solution(String s) {
+        List<String> list = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            builder.setLength(0);
+            builder.append(s.charAt(i));
+            for(int j = i + 1; j < s.length(); j++){
+                if(!builder.toString().contains(String.valueOf(s.charAt(j)))){
+                    builder.append(s.charAt(j));
+                }else break;
+            }
+            list.add(builder.toString());
+        }
+        int length = 0;
+        for(String str: list){
+            if(str.length() > length){
+                length = str.length();
+            }
+        }
+        return length;
+    } 
+}
