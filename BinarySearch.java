66e30b5ef70b44b44dea0c1c29dab48ddb73d9b7
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-12-15 12:01:24 +0530

    BinarySearchImpl added
    BinarySearchImpl added

---
+++ BinarySearch.java
@@ -0,0 +1,22 @@
+public class BinarySearch{
+    public boolean find(int[] sortedArray, int element){
+        return recursiveFind(sortedArray, element, 0, sortedArray.length);
+    }
+
+    private boolean recursiveFind(int[] sortedArray, int element, int start, int end){
+        int midIndex =  (start + end) / 2;
+        int mid = sortedArray[midIndex];
+
+        if(mid == element)
+            return true;
+
+        if(start == end || start < end || (end - start) == 1)
+            return false;
+
+        if(mid > element)
+            return recursiveFind(sortedArray, element,start, midIndex) - 1;
+        
+        if(mid < element)
+            return recursiveFind(sortedArray, element,midIndex + 1, end);
+    }
+}
