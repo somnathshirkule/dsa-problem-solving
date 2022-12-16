415d96a5b6dbf63c6294947b017a3d0b206b1113
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-12-16 20:41:31 +0530

    Update WayToReachNthStair.java

--- WayToReachNthStair.java
+++ WayToReachNthStair.java
@@ -6,7 +6,7 @@ public class WayToReachNthStair{
             return 1;
         if(n == 2)
             return 2;
-            
+        //Steps can be taken are -> 1, 2, 3
         return getWays(n-1) + getWays(n-2) + getWays(n-3);
     }
 }
