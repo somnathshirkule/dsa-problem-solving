bc2f97c36a2f9f53904f0b2248bf4afa2135bdb6
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-12-25 10:33:35 +0530

    Create WaysToReachNthStair.java

---
+++ WaysToReachNthStair.java
@@ -0,0 +1,23 @@
+public class WaysToReachNthStair{
+    public List<String> getWays(int n){
+        //n is the number of stair Nth stair.
+
+        List<String> ways = new ArrayList<>();
+        getRecusrsively(ways, n, "",n);
+        return ways;
+    }
+
+    private void getRecusrsively(List<String> ways, int n, String way, int current){
+        if(current == 0){
+            ways.add(way);
+            return;
+        }
+
+        if(current < 0)
+            return;
+
+        getRecusrsively(ways, n, way + "1", current-1);
+        getRecusrsively(ways, n, way + "2", current-2);
+        getRecusrsively(ways, n, way + "3", current-3);
+    }
+}
